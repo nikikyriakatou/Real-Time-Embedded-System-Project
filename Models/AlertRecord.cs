08d7b433f9a63054711b9ce87764bd2bcc939150
nikikyriakatou <103365762+nikikyriakatou@users.noreply.github.com>
2026-03-05 14:55:14 +0200

    Delete Models directory

--- Models/AlertRecord.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Models;
-
-public sealed record AlertRecord
-{
-	public string Outpost { get; set; } = string.Empty;
-	public string Alert { get; set; } = string.Empty;
-	public double StartingCoordX { get; set; }
-	public double StartingCoordY { get; set; }
-}
